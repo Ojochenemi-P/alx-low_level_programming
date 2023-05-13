7da555cd3649e65eb2c8dac3a6621d21e1a0e050
Ojochenemi-P <abuojochenemi32@gmail.com>
2023-05-13 11:01:44 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,45 +2,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 /**
- * main - Prints the sum of args positive numbers
- * @argc: argument count
- * @argv: argument vector
+ * check_num - check - string there are digit
+ * @str: array str
  *
- * Return: Always zero
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
+	{
+		if (!isdigit(str[count]))
+			/*check if str there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always o (Success)
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	unsigned int k, sum = 0;
-	char *e;
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-	if (argc > 1)
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
 	{
-		for (i = 1; i < argc; i++)
+		if (check_num(argv[count]))
+
 		{
-			e = argv[i];
-
-			for (k = 0; strlen(e); k++)
-			{
-				if (e[k] < 48 || e[k] > 57)
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-
-			sum += atoi(e);
-			e++;
+			str_to_int = atoi(argv[count]); /*ATOI -> convert string to int*/
+			sum += str_to_int;
 		}
 
-		printf("%d\n", sum);
+		/*Condition if one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
 	}
-	else
-	{
-		printf("0\n");
-	}
+
+	printf("%d\n", sum); /*print sum*/
 
 	return (0);
 }

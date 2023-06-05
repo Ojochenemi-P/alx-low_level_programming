ee16b495b725ded779b9d9ae86ad452198ffefb2
Ojochenemi-P <Ojochenemi-P>
2023-06-05 03:32:36 -0700

    Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * pop_listint - Deletes the head node of linked list
+ * @head: Pointer to first element in linked list
+ *
+ * Return: The data inside the elements that was deleted,
+ * or 0 if the list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (!head || !*head)
+		return (0);
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (num);
+}
+
